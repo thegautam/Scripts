05750ef733e38617fb9fa0807f0595bcea147cd6
Gautam Kedia <gautamkedia@gmail.com>
2011-03-05 17:30:30 -0800

    Script to download images from Bing.

---
+++ download-bing-images.sh
@@ -0,0 +1,12 @@
+img_dir=~/Pictures/Bing
+mkdir -p $img_dir
+
+curl "http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US" | egrep -o "http:[^\"]*jpg" | while read url
+do
+    wurl=${url// /%20}
+    file=$img_dir`expr "$url" : ".*\(/.*\)"`
+    if [ ! -a "$file" ]
+    then
+        curl "$wurl" > "$file"
+    fi
+done

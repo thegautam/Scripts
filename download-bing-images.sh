d08f61e7679421bd80164f4a129d2e136d35221f
Gautam Kedia <gautamkedia@gmail.com>
2011-03-06 14:57:09 -0800

    No duplicate checks & better formatting.

--- download-bing-images.sh
+++ download-bing-images.sh
@@ -1,15 +1,18 @@
 img_dir=~/Pictures/Bing
 mkdir -p $img_dir
 
-curl "http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US" 2>/dev/null | egrep -o "http:[^\"]*(JPG|jpg)" | while read url
+curl "http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US" 2>/dev/null | 
+    egrep -o "ref=\"[^\"]*\"" | 
+    egrep -o "http:[^\"]*(JPG|jpg)" | 
+    while read url
 do
     wurl=${url// /%20}
     file=$img_dir`expr "$url" : ".*\(/.*\)"`
- if [ -a "$file" ]
- then
-     echo "$file" exists.
- else
-     echo "$file" downloading.
-     curl "$wurl" > "$file" 2>/dev/null
- fi
+    if [ -a "$file" ]
+    then
+        echo "$file" exists.
+    else
+        echo "$file" downloading.
+        curl "$wurl" > "$file" 2>/dev/null
+    fi
 done

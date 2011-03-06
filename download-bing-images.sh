a1dfd0143b41045d3c53d5d81c6d6aecd2f55749
Gautam Kedia <gautamkedia@gmail.com>
2011-03-05 17:37:01 -0800

    Fixing regex.

--- download-bing-images.sh
+++ download-bing-images.sh
@@ -1,7 +1,7 @@
 img_dir=~/Pictures/Bing
 mkdir -p $img_dir
 
-curl "http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US" | egrep -o "http:[^\"]*jpg" | while read url
+curl "http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US" | egrep -o "http:[^\"]*(JPG|jpg)" | while read url
 do
     wurl=${url// /%20}
     file=$img_dir`expr "$url" : ".*\(/.*\)"`

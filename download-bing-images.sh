402eea23638feae96d6bf89baf2e683af07a46cb
Gautam <gautamkedia@gmail.com>
2011-03-09 00:19:08 -0800

    Added comments.

--- download-bing-images.sh
+++ download-bing-images.sh
@@ -3,18 +3,28 @@ set -e
 img_dir=~/Pictures/Bing
 mkdir -p $img_dir
 
-curl "http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US" 2>/dev/null | 
+curl "http://themeserver.microsoft.com/default.aspx?p=Bing&c=Desktop&m=en-US" 2>/dev/null |
+
+    # Find a ref node.
     egrep -o "ref=\"[^\"]*\"" | 
+
+    # Extract the url from it.
     egrep -o "http:[^\"]*(JPG|jpg)" | 
+
     while read url
-do
-    wurl=${url// /%20}
-    file=$img_dir`expr "$url" : ".*\(/.*\)"`
-    if [ -a "$file" ]
-    then
-        echo "$file" exists.
-    else
-        echo "$file" downloading.
-        curl "$wurl" > "$file" 2>/dev/null
-    fi
-done
+    do
+        # Make the url web ready.
+        wurl=${url// /%20}
+
+        # Image name is last part of url.
+        file=$img_dir`expr "$url" : ".*\(/.*\)"`
+
+        # Download only if file is missing.
+        if [ -a "$file" ]
+        then
+            echo "$file" exists.
+        else
+            echo "$file" downloading.
+            curl "$wurl" > "$file" 2>/dev/null
+        fi
+    done
